cb36b72661a9c85e81c3ae49bc65360312042774
Timo Kokkonen <tjko@iki.fi>
2012-12-24 16:39:59 -0800

    fix the clearing of context buffer path by Stefan Farfeleder

--- md5.c
+++ md5.c
@@ -167,7 +167,7 @@ MD5Final(digest, ctx)
 	MD5Transform(ctx->buf, (uint32 *)ctx->in);
 	byteReverse((unsigned char *)ctx->buf, 4);
 	memcpy(digest, ctx->buf, 16);
-	memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
+	memset(ctx, 0, sizeof(*ctx));	/* In case it's sensitive */
 }
 
 #ifndef ASM_MD5
